a01c33da53507d25a493a5d673ecfab7d4e32542
806-17 <8o617@41cs806-17.cooper.edu>
2012-11-15 19:55:34 -0500

    switching directions

--- server.js
+++ server.js
@@ -1,59 +1,21 @@
 var express = require('express');
 var sys = require('sys');
 var oauth = require('oauth');
+var util = require('util'),
+var twitter = require('twitter');
 
 var app = express();
 
-var _twitterConsumerKey = "0AryNjToIG2Alt7Sdc31gg";
-var _twitterConsumerSecret = "1xsJUvzU2MX75BMFEpq8MQRBxkgik57HiMG1CuQsUs";
-
-function consumer() {
-  return new oauth.OAuth(
-    "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token", 
-    _twitterConsumerKey, _twitterConsumerSecret, "1.0A", "http://badgestar.com/sessions/callback", "HMAC-SHA1");   
-}
-
-
-app.use(express.cookieSession({"secret":"twitter"}));
-
-
-app.get('/', function(req, res){
-  res.send('Hello World');
+var twit = new twitter({
+    consumer_key: '0AryNjToIG2Alt7Sdc31gg',
+    consumer_secret: '1xsJUvzU2MX75BMFEpq8MQRBxkgik57HiMG1CuQsUs',
+    access_token_key: '1465161-zbfgSiTZmeRSCrgu28BTNZo9eEbSFq5ouhMSfNUD4g',
+    access_token_secret: 'Y2RKVQ3wKONXBDuZofpCYmq0O2ALOwzkZjpqxghgs'
 });
 
-app.get('/sessions/connect', function(req, res){
-  consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
-    if (error) {
-      res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
-    } else {  
-      req.session.oauthRequestToken = oauthToken;
-      req.session.oauthRequestTokenSecret = oauthTokenSecret;
-      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
-    }
-  });
+twit.get('/statuses/show/27593302936.json', {include_entities:true}, function(data) {
+    console.log(util.inspect(data));
 });
 
-app.get('/sessions/callback', function(req, res){
-  sys.puts(">>"+req.session.oauthRequestToken);
-  sys.puts(">>"+req.session.oauthRequestTokenSecret);
-  sys.puts(">>"+req.query.oauth_verifier);
-  consumer().getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
-    if (error) {
-      res.send("Error getting OAuth access token : " + sys.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+sys.inspect(results)+"]", 500);
-    } else {
-      req.session.oauthAccessToken = oauthAccessToken;
-      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
-      // Right here is where we would write out some nice user stuff
-      consumer.get("http://twitter.com/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
-        if (error) {
-          res.send("Error getting twitter screen name : " + sys.inspect(error), 500);
-        } else {
-          req.session.twitterScreenName = data["screen_name"];    
-          res.send('You are signed in: ' + req.session.twitterScreenName)
-        }  
-      });  
-    }
-  });
-});
 
 app.listen(parseInt(process.env.PORT || 80));
