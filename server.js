87ec12496db070207ab8bdb83824c82e4a2a2f64
Erin Sparling <erin@everyplace.net>
2012-11-29 22:00:04 -0500

    Added twitter files.

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-// var express = require('express');
-// var oauth = require('oauth');
+var express = require('express');
+var oauth = require('oauth');
 var util = require('util');
 var twitter = require('twitter');
 
-// var app = express();
+var app = express();
 
 var twit = new twitter({
-    consumer_key: '0AryNjToIG2Alt7Sdc31gg',
-    consumer_secret: '1xsJUvzU2MX75BMFEpq8MQRBxkgik57HiMG1CuQsUs',
-    access_token_key: '1465161-zbfgSiTZmeRSCrgu28BTNZo9eEbSFq5ouhMSfNUD4g',
-    access_token_secret: 'Y2RKVQ3wKONXBDuZofpCYmq0O2ALOwzkZjpqxghgs'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
 });
 
-twit.get('/statuses/show/27593302936.json', {include_entities:true}, function(data) {
+twit.get('/statuses/retweets/273795885580816384.json', {include_entities:true}, function(data) {
     console.log(util.inspect(data));
 });
 
